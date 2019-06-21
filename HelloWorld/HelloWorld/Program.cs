1d5f0ef199185eb263f7fdf1ea17fa74561d1bc0
Saijitsu <zeromus16@msn.com>
2019-06-21 14:49:02 +0200

    Hello world CSharp

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+// A Hello World! program in C#.
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
