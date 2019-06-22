7fe9135810a3e17b937e33e0fa33843859cf070e
Saijitsu <zeromus16@msn.com>
2019-06-22 14:42:14 +0200

    add typecoversion test

---
+++ TypeConversion/TypeConversion/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 12345;
+            // byte b = i; can't compile (only cast option)
+            byte b = (byte) i;
+            Console.WriteLine(b);
+        }
+    }
+}
