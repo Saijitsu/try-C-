398b48d6cd3df360a1869e5f54778dc068f0114c
Saijitsu <zeromus16@msn.com>
2019-06-24 16:32:16 +0200

    Classes try

---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Classes
+{
+    class Program {
+    static void Main(string[] args)
+        {
+        var john = new Person();
+        john.FirstName = "John";
+        john.LastName = "Smith";
+        john.Introduce();
+
+        /*var john = new Person
+        {
+            FirstName = "John",
+            LastName = "Smith"
+        };*/
+
+        }
+    }
+}
