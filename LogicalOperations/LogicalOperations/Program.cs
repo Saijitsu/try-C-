3c82b7ee2fd9b6d10a319979f6fa2626fba5f5e2
Saijitsu <zeromus16@msn.com>
2019-06-24 15:57:04 +0200

    Test on Logical Opérations

--- LogicalOperations/LogicalOperations/Program.cs
+++ LogicalOperations/LogicalOperations/Program.cs
@@ -6,7 +6,14 @@ namespace LogicalOperations
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var a = 1;
+            var b = 2;
+            // Console.WriteLine((float)a / (float)b); => if not float = 3
+           // Console.WriteLine(a != b);
+            Console.WriteLine(!(a != b)); // 2 negative, positive, not easy to read
+            var c = 3;
+            Console.WriteLine(c> b && c == a); // True & False => False
+            Console.WriteLine(c > b || c == a); // if One is True => True
         }
     }
 }
