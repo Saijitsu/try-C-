0b077bb6da26b9c119b275e6da71e16a72212908
Saijitsu <zeromus16@msn.com>
2019-06-25 10:27:42 +0200

    try on strings

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstName = "Nicolas"; //class.System.String
+           // String lastname = "T";
+           var lastName = "T";
+
+           var fullName = firstName + " " + lastName;
+           Console.WriteLine(fullName);
+           var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
+           Console.WriteLine(myFullName);
+
+           var names = new string[3] {"Nicolas", "Michel", "Julio"};
+           var formattedNames = string.Join(", ", names);
+           Console.WriteLine(formattedNames);
+        }
+    }
+}
